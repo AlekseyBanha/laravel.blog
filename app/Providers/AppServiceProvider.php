d3886c13f253de30969e209c7bdbbaaa1eb10eb4
bandito228 <92264227+AlekseyBanha@users.noreply.github.com>
2022-01-16 22:33:30 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,35 +27,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        if (Cache::has('categories')) {
-//            $categories = Cache::get('categories');
-//        } else {
-//            $categories = Category::with('posts')->withCount('posts')
-//                ->orderBy('posts_count', 'desc')->get();
-//            Cache::put('categories', $categories, 30);
-//        }
-//        View::share('categories', $categories);
-//
-//        view()->composer('layouts.sidebar', function ($view) {
-//            $view->with('popular_posts', Post::orderBy('view', 'desc')->limit(5)->get());
-//
-//        });
-//
-//        if (Cache::has('news_post')) {
-//            $news_post = Cache::get('news_post');
-//        } else {
-//            $news_post = Post::orderBy('id', 'desc')->limit(5)->get();
-//            Cache::put('news_post', $news_post, 300);
-//        }
-//        View::share('news_post', $news_post);
-//
-//        if (Cache::has('popular_post')) {
-//            $popular_post = Cache::get('popular_post');
-//        } else {
-//            $popular_post = Post::orderBy('view', 'desc')->limit(5)->get();
-//            Cache::put('popular_post', $popular_post, 300);
-//        }
-//        View::share('popular_post', $popular_post);
+        if (Cache::has('categories')) {
+            $categories = Cache::get('categories');
+        } else {
+            $categories = Category::with('posts')->withCount('posts')
+                ->orderBy('posts_count', 'desc')->get();
+            Cache::put('categories', $categories, 30);
+        }
+        View::share('categories', $categories);
+
+        view()->composer('layouts.sidebar', function ($view) {
+            $view->with('popular_posts', Post::orderBy('view', 'desc')->limit(5)->get());
+
+        });
+
+        if (Cache::has('news_post')) {
+            $news_post = Cache::get('news_post');
+        } else {
+            $news_post = Post::orderBy('id', 'desc')->limit(5)->get();
+            Cache::put('news_post', $news_post, 300);
+        }
+        View::share('news_post', $news_post);
+
+        if (Cache::has('popular_post')) {
+            $popular_post = Cache::get('popular_post');
+        } else {
+            $popular_post = Post::orderBy('view', 'desc')->limit(5)->get();
+            Cache::put('popular_post', $popular_post, 300);
+        }
+        View::share('popular_post', $popular_post);
 
     }
 }
